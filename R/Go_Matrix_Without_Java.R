986a11c0c4c0e1912b5f5c43fd56911acac12e9d
cparsania <chirag.parsania@gmail.com>
2016-11-21 17:22:18 +0800

    Updated design and bug fix

--- R/Go_Matrix_Without_Java.R
+++ R/Go_Matrix_Without_Java.R
@@ -1,46 +1,11 @@
-
-
-matrixFile="goMatrix.txt"
-data <- read.table(matrixFile,sep="\t",quote=NULL, comment='',check.names=F)
-head(data)
-
-dat <- sapply(as.character(data[,2]), function(X){return(strsplit(X,":"))}) ## split by deleminator 
-names(dat) <- as.character(data[,1]) ## assign names to each elem of list 
-
-mat <- matrix(nrow = length(dat),ncol = length(dat))
-colnames(mat) <- names(dat)
-row.names(mat) <- names(dat)
-
-combinations <- combn(c(1:length(dat)),2)
-numberOfGenesOverlapped  <- apply(combinations, 2, function(X) { 
-        row = X[1] 
-        col = X[2] 
-        return(length(intersect(dat[[row]],dat[[col]])))
+################################
+## Plot Function 
+################################
+doGoCorrPlot<- function(data,pref = "out"){
         
-})
-
-
-## assign value to matrix.
-## chk 
-for(i in c(1: ncol(combinations))){
-        indx <- combinations[,i]
-        mat[indx[1],indx[2]] <- numberOfGenesOverlapped[i]
-        mat[indx[2],indx[1]] <- numberOfGenesOverlapped[i]
-}
-
-
-## assign value to diagonal elements 
-for(i in c(1: dim(mat)[1])){
         
-        mat[i,i] <- 1
-}
-pref ="40_updated"
-doGoCorrPlot(mat)
-doGoCorrPlot<- function(data){
         # data inform of data.frame. input will be output of java program GoMatrix. Manually you have to add last column as label in the input file .
-        
         library(corrplot)
-        
         ## prepare lebel from given input file 
         data <- mat
         ## Do clustering. rearrange matrix row and column before clustering.
@@ -61,3 +26,49 @@ doGoCorrPlot<- function(data){
         write.table(rownames(x),file = paste(pref,"MyGoClusterMap.txt",sep="_"),sep = "\t",quote = F)
         
 }
+
+################################
+## Process input data 
+################################
+
+matrixFile="goMatrix.txt" ## input file contains two column. Go and associated genes. Genes MUST BE sepereted by ":"
+data <- read.table(matrixFile,sep="\t",quote=NULL, comment='',check.names=F)
+head(data)
+
+dat <- sapply(as.character(data[,2]), function(X){return(strsplit(X,":"))}) ## split by deleminator 
+names(dat) <- as.character(data[,1]) ## assign names to each elem of list 
+
+mat <- matrix(nrow = length(dat),ncol = length(dat))
+colnames(mat) <- names(dat)
+row.names(mat) <- names(dat)
+
+combinations <- combn(c(1:length(dat)),2)
+numberOfGenesOverlapped  <- apply(combinations, 2, function(X) { 
+        row = X[1] 
+        col = X[2] 
+        return(length(intersect(dat[[row]],dat[[col]])))
+        
+})
+
+## assign value to matrix.
+## chk 
+for(i in c(1: ncol(combinations))){
+        indx <- combinations[,i]
+        mat[indx[1],indx[2]] <- numberOfGenesOverlapped[i]
+        mat[indx[2],indx[1]] <- numberOfGenesOverlapped[i]
+}
+
+## assign value to diagonal elements 
+for(i in c(1: dim(mat)[1])){
+        
+        mat[i,i] <- 1
+}
+
+prefix ="40_updated" ## prefix for output plot 
+
+################################
+## Run Function
+################################
+
+doGoCorrPlot(mat , pref = prefix)
+

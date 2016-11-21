c16f5c63d4da85dc6f0f3bf5994449a0582fe505
cparsania <chirag.parsania@gmail.com>
2016-11-21 16:51:16 +0800

    First Commit

---
+++ R/Go_Matrix_Without_Java.R
@@ -0,0 +1,62 @@
+
+
+matrixFile="goMatrix.txt"
+data <- read.table(matrixFile,sep="\t",quote=NULL, comment='',check.names=F)
+head(data)
+
+dat <- sapply(as.character(data[,2]), function(X){return(strsplit(X,":"))}) ## split by deleminator 
+names(dat) <- as.character(data[,1]) ## assign names to each elem of list 
+
+mat <- matrix(nrow = length(dat),ncol = length(dat))
+colnames(mat) <- names(dat)
+row.names(mat) <- names(dat)
+
+combinations <- combn(c(1:length(dat)),2)
+numberOfGenesOverlapped  <- apply(combinations, 2, function(X) { 
+        row = X[1] 
+        col = X[2] 
+        return(length(intersect(dat[[row]],dat[[col]])))
+        
+})
+
+
+## assign value to matrix 
+for(i in c(1: ncol(combinations))){
+        indx <- combinations[,i]
+        mat[indx[1],indx[2]] <- numberOfGenesOverlapped[i]
+        mat[indx[2],indx[1]] <- numberOfGenesOverlapped[i]
+}
+
+
+## assign value to diagonal elements 
+for(i in c(1: dim(mat)[1])){
+        
+        mat[i,i] <- 1
+}
+pref ="40_updated"
+doGoCorrPlot(mat)
+doGoCorrPlot<- function(data){
+        # data inform of data.frame. input will be output of java program GoMatrix. Manually you have to add last column as label in the input file .
+        
+        library(corrplot)
+        
+        ## prepare lebel from given input file 
+        data <- mat
+        ## Do clustering. rearrange matrix row and column before clustering.
+        M = cor(data, method="pearson") # do correlation 
+        go_clst<-hclust(dist(M)) #do clustering of correlated data
+        x<-M[go_clst$order,] # rearrange matrix with clustering order, rearrange row first
+        x<-x[,go_clst$order] # rearrange matrix with clustering order, rearrange column now 
+        head(colnames(x)) # chk arranged columns 
+        head(rownames(x)) # chk arranged rows 
+        attributes(go_clst)  
+        
+        ## generate plot 
+        png(file=paste(pref,"MyGoClusterMap.png",sep="_"),height = 5000,width=5000) # create file
+        corrplot(as.matrix(x), type="full", method="color",is.corr = F,diag=T,order ="original",tl.cex=1,cl.cex = 5) #do corr plot without  clustering & for single sample 
+        #legend("topleft", legend = names(s), col=s, pch="---",cex=5,text.col = s)  # put legend on plot
+        dev.off()
+        ## write go in order of clustering 
+        write.table(rownames(x),file = paste(pref,"MyGoClusterMap.txt",sep="_"),sep = "\t",quote = F)
+        
+}

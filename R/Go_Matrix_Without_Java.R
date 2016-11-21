810d5da60c8582a6275f5124a18f6b014511de7c
cparsania <chirag.parsania@gmail.com>
2016-11-21 17:12:34 +0800

    Commnets Updated

--- R/Go_Matrix_Without_Java.R
+++ R/Go_Matrix_Without_Java.R
@@ -20,7 +20,7 @@ numberOfGenesOverlapped  <- apply(combinations, 2, function(X) {
 })
 
 
-## assign value to matrix 
+## assign value to matrix.
 for(i in c(1: ncol(combinations))){
         indx <- combinations[,i]
         mat[indx[1],indx[2]] <- numberOfGenesOverlapped[i]
